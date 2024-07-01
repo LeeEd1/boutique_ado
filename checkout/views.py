0a198ba0fd8aa87abcd4d41dcd92ec2e397d7f28
Lee Edwards <edwardsla@live.co.uk>
2024-07-01 16:54:47 +0000

    adding stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51PXmVcIN7CmKmCnx9jFQV6ZZgvbOTctcK6jYqtz10k99GUezBcRgZ9qZgmTXzaSlwCSWV1ksVIGjZauUzSqAVBFW00CxC7hsLz',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
